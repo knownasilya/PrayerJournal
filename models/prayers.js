dc7009aaf533cbf10cbe5cd5bfc1f94cf56d6db7
Ilya Radchenko <ilya@burstcreations.com>
2013-01-18 23:12:27 -0500

    Added Needs and Prayers' allows.

--- models/prayers.js
+++ models/prayers.js
@@ -1,1 +1,35 @@
-Prayers = new Meteor.Collection("prayers");
+
+/* 
+ * Each Prayer is represented by a document in the Prayers Collection:
+ *   owner: user id
+ *   prayer: The owners prayer
+ *   answer: God's answer
+ *   public: boolean
+ *   shared_with: Array of Users, only if not public
+ *   date_created: Date
+ *   date_answered: Date
+ *   estimated_duration: Time
+ */
+Prayers = new Meteor.Collection( "prayers" );
+Needs = new Meteor.Collection( "needs" );
+
+Prayers.allow({
+  insert: function ( userId, prayer ) {
+    return false; // Use createPrayer to insert
+  },
+  update: function ( userId, prayer, answer ) {
+    return checkUser( userId, prayer );
+  },
+  remove: function ( userId, prayer ) {
+    return checkUser( userId, prayer );
+  }
+});
+
+var checkUser = function ( userId, prayer ) {
+  if( userId !== prayer.owner ) {
+    return false;
+  }
+  return true;
+}
+
+
